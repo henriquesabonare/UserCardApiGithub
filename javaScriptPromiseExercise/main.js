8cff55c6d97aebdaa064e002794158e19024fd7c
Henrique Lourenço Sabonare <henriquesabonare0611@gmail.com>
2022-07-01 12:32:10 -0300

    update promise js and stylesheet

--- javaScriptPromiseExercise/main.js
+++ javaScriptPromiseExercise/main.js
@@ -1,4 +1,4 @@
-showFollowersInHtml()
+// showFollowersInHtml()
 
 showImgInHtml()
 
--- javaScriptPromiseExercise/services.js
+++ javaScriptPromiseExercise/services.js
@@ -1,37 +1,80 @@
 function showImgInHtml() {
   Promise.all([
-    axios.get("https://api.github.com/users/henriquesabonare")
+    axios.get("https://api.github.com/users/henriquesabonare"),
+    axios.get('https://api.github.com/users/henriquesabonare/followers'),
+    axios.get('https://api.github.com/users/henriquesabonare/following'),
+    axios.get('https://api.github.com/users/henriquesabonare/repos')
   ])
   
     .then((res) => {
-      const img = () => res[0].data.avatar_url;
-
-      function showImg(img) {
+      function showImg() {
+        const img = res[0].data.avatar_url;
         return (document.querySelector(
           ".imgs"
         ).innerHTML = `<img src='${img}'>`);
       }
-      showImg(img());
-    })
-    .catch((error) => console.log(error));
-}
-function showFollowersInHtml(){
-  Promise.all([
-    axios.get('https://api.github.com/users/henriquesabonare/followers')
-  ])
-
-  .then(
-    (res) => {
-    const followers = () => res[0].data.length
-      function showFollowers(followers){
-        console.log()
+      
+      function showFollowers(){
+        const followers = res[1].data.length
         let dados = document.querySelector('.data')
         let h3 = document.createElement('h3')
         dados.appendChild(h3)
         return h3.textContent = `${followers} Seguidores`
       }
-      showFollowers(followers())
-
-    }
-  )
+      function showFollowing(){
+        const following = res[2].data.length
+        let dados = document.querySelector('.data')
+        let h3 = document.createElement('h3')
+        dados.appendChild(h3)
+        return h3.textContent = `${following} Seguindo`
+      }
+      function showRepos(){
+        const repos = res[3].data.length
+        let dados = document.querySelector('.data')
+        let h3 = document.createElement('h3')
+        dados.appendChild(h3)
+        return h3.textContent = `${repos} Repositórios`
+      }
+      function showCompany(){
+        const company = res[0].data.company
+        let dados = document.querySelector('.data')
+        let h3 = document.createElement('h3')
+        dados.appendChild(h3)
+        return h3.textContent = `${company}`
+      }
+      function showLocation(){
+        const location = res[0].data.location.replace(', São Bernardo do Campo ', ' - ')
+        let dados = document.querySelector('.data')
+        let h3 = document.createElement('h3')
+        dados.appendChild(h3)
+        return h3.textContent = `${location}`
+      }
+      showImg()
+      showFollowers()
+      showFollowing()
+      showRepos()
+      showCompany()
+      showLocation()
+    })
+    .catch((error) => console.log(error));
 }
+// function showFollowersInHtml(){
+//   Promise.all([
+//     axios.get('https://api.github.com/users/henriquesabonare/followers')
+//   ])
+
+//   .then(
+//     (res) => {
+//     const followers = () => res[0].data.length
+//       function showFollowers(followers){
+//         console.log()
+//         let dados = document.querySelector('.data')
+//         let h3 = document.createElement('h3')
+//         dados.appendChild(h3)
+//         return h3.textContent = `${followers} Seguidores`
+//       }
+//       showFollowers(followers())
+
+//     }
+//   )
+// }
